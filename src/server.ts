3f7aac427ba97f606388c545d16c744bd5fd6942
Fulton Zink <joeyzink@gmail.com>
2020-09-21 13:55:49 -0700

    Add server file for express server. Add scripts to package.json.

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const port: number = 3000;
+
+app.get('/', (req, res) => {
+    res.send('The app is working :)');
+});
+
+app.listen(port, () => {
+    return console.log(`The server is listening on port ${port}`);
+})
